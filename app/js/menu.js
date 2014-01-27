49f0a2bf6cf4060e32566dfefbd6458ba28e8e23
Rudolf Hersen <rhersen@gmail.com>
2014-01-27 17:20:01 +0100

    converted stationLink function to React component

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,30 +1,28 @@
 var MainMenu = React.createClass({
     render: function() {
-        var stations = {
-            KBG: '9510',
-            STE: '9520',
-            TLG: '9525',
-            SDR: '9530'
-        };
+        var stations = [
+            '9510',
+            '9520',
+            '9525',
+            '9530'
+        ];
 
-        return React.DOM.nav({children: _.map(stations, stationLink)});
+        return React.DOM.nav({children: _.map(stations, function (number) {
+            return StationLink({number: number});
+        })});
     }
 });
 
 var RefreshMenu = React.createClass({
     getStations: function(c) {
-        var stations = {};
-
-        stations[c - 1] = c - 1;
-        stations[names.abbreviate(_.first(this.props.trains).StopAreaName)] = c;
-        stations[c + 1] = c + 1;
-
-        return stations;
+        return [c - 1, c, c + 1];
     },
 
     render: function() {
         var stations = this.getStations(this.props.current);
 
-        return React.DOM.nav({children: _.map(stations, stationLink)});
+        return React.DOM.nav({children: _.map(stations, function (number) {
+            return StationLink({number: number});
+        })});
     }
 });
--- app/js/station-link.js
+++ app/js/station-link.js
@@ -1,15 +1,17 @@
-function stationLink(number, name) {
+var StationLink = React.createClass({
 
-    function handleResult(resultTrains) {
+    handleResult: function(resultTrains) {
         reactRoot.setState({responseTime: new Date().getTime()});
         reactRoot.setState({trains: resultTrains});
         reactRoot.setState({current: parseInt(_.first(resultTrains).SiteId, 10)});
-    }
+    },
 
-    function sendRequest(id) {
+    sendRequest: function(id) {
+        var self = this;
+
         function callback() {
             if (ajax.readyState === 4 && ajax.status === 200) {
-                handleResult(JSON.parse(ajax.responseText));
+                self.handleResult(JSON.parse(ajax.responseText));
             }
         }
 
@@ -20,21 +22,24 @@ function stationLink(number, name) {
         ajax.onreadystatechange = callback;
         ajax.open("GET", '/departures/' + id, true);
         ajax.send();
+    },
+
+    render: function() {
+        var self = this;
+
+        return React.DOM.span({
+                className: 'siteid',
+                onClick: function() {
+                    function tick() {
+                        reactRoot.setState({now: new Date()});
+                    }
+
+                    self.sendRequest(self.props.number);
+
+                    if (!reactRoot.state.intervalId) {
+                        reactRoot.setState({intervalId: setInterval(tick, 256)});
+                    }
+                } },
+            this.props.number);
     }
-
-    return React.DOM.span({
-            id: name,
-            className: 'siteid',
-            onClick: function() {
-                function tick() {
-                    reactRoot.setState({now: new Date()});
-                }
-
-                sendRequest(number);
-
-                if (!reactRoot.state.intervalId) {
-                    reactRoot.setState({intervalId: setInterval(tick, 256)});
-                }
-            } },
-        name);
-}
+});
