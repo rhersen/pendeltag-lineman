7193a689b210a3f2a431f064d14bc2746e29bed9
Rudolf Hersén <rudolf@qwaya.com>
2015-01-13 07:48:02 +0100

    ported MainMenu from coffee to lodash

---
+++ app/js/MainMenu.js
@@ -0,0 +1,71 @@
+window.MainMenu = React.createClass({
+    getInitialState: function () {
+        return {
+            stations: {
+                northwest: [],
+                northeast: [],
+                central: [],
+                southwest: [],
+                southeast: []
+            }
+        };
+    },
+    componentDidMount: function () {
+        var request;
+        var self = this;
+
+        request = new XMLHttpRequest();
+        request.open('GET', '/stations', true);
+        request.onload = function () {
+            if (this.status >= 200 && this.status < 400) {
+                self.handleStations(JSON.parse(this.response));
+            }
+        };
+        request.send();
+    },
+    handleStations: function (stations) {
+        this.setState({
+            stations: stations
+        });
+    },
+
+    render: function () {
+        return React.DOM.div({},
+            React.DOM.nav({
+                className: 'pull-left',
+                children: _.map(_.initial(this.state.stations.northwest, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'pull-right',
+                children: _.map(_.initial(this.state.stations.northeast, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'pull-left narrow',
+                children: _.map(_.last(this.state.stations.northwest, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'center',
+                children: _.map(_.first(this.state.stations.central, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'pull-right narrow',
+                children: _.map(_.last(this.state.stations.northeast, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'center wide',
+                children: _.map(_.rest(_.initial(this.state.stations.central)), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'pull-left narrow',
+                children: _.map(this.state.stations.southwest, StationLink)
+            }),
+            React.DOM.nav({
+                className: 'center',
+                children: _.map(_.last(this.state.stations.central, 1), StationLink)
+            }),
+            React.DOM.nav({
+                className: 'pull-right narrow',
+                children: _.map(this.state.stations.southeast, StationLink)
+            }));
+    }
+});
