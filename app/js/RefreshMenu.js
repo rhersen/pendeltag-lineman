94abb646f747605773bad582c721c85bab95df7a
Rudolf Hersén <rudolf@qwaya.com>
2015-01-29 20:56:42 +0100

    ported RefreshMenu from coffee to js

---
+++ app/js/RefreshMenu.js
@@ -0,0 +1,20 @@
+var RefreshMenu = React.createClass({
+    render: function () {
+        var c;
+        c = this.props.current;
+        return React.DOM.nav({
+            className: 'refresh'
+        }, StationLink({
+            SiteId: c - 1
+        }), StationLink({
+            className: 'current',
+            SiteId: c,
+            StopAreaName: this.props.StopAreaName
+        }), StationLink({
+            SiteId: c + 1
+        }), React.DOM.div({
+            className: 'link',
+            onClick: this.props.clear
+        }, 'Klar'));
+    }
+});
